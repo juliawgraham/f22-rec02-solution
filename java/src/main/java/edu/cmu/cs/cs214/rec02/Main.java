159801710539ffac4ffa650294370a14a3ccad93
juliawgraham <juliagra@andrew.cmu.edu>
2022-09-03 16:52:40 -0400

    java/ts information hiding solutions

--- java/src/main/java/edu/cmu/cs/cs214/rec02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/rec02/Main.java
@@ -4,15 +4,11 @@ import edu.cmu.cs.cs214.rec02.points.*;
 
 public class Main {
     public static void main(String[] args) {
-        PolarPoint point1 = new PolarPoint(2, 0);
-        PolarPoint point2 = new PolarPoint(4, Math.PI);
+        Point point1 = new PolarPoint(2, 0);
+        Point point2 = new PolarPoint(4, Math.PI);
         
         Line line = new Line(point1, point2);
         
         line.draw();
-
-        System.out.println(point1.len);
-        point1.len = 3;
-        System.out.println(point1.len);
     }
 }
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,13 +1,9 @@
 import { newLine } from "./line";
-import { PolarPoint, newPolarPoint } from "./points/polarPoint";
+import { newPolarPoint } from "./points/polarPoint";
 
-const point1: PolarPoint = newPolarPoint(2, 0);
-const point2: PolarPoint = newPolarPoint(4, Math.PI);
+const point1: Point = newPolarPoint(2, 0);
+const point2: Point = newPolarPoint(4, Math.PI);
 
 const line = newLine(point1, point2);
 
 line.draw();
-
-console.log(point1.len);
-point1.len = 3;
-console.log(point1.len);
